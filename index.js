05c7d7319a9b03f548d590e020150b502e8ca6d1
Brandon Pittman <brandon@brandonpittman.net>
2019-12-24 11:02:50 +0900

    wip

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-module.exports = function ({addUtilities}) {
-  let colors = {
-    "accent-1": "#FAFAFA",
-    "accent-2": "#EAEAEA",
-    "accent-3": "#999",
-    "accent-4": "#888",
-    "accent-5": "#666",
-    "accent-6": "#444",
-    "accent-7": "#333",
-    "accent-8": "#111",
-    "error-light": "#FF1A1A",
-    "error": "#E00",
-    "error-dark": "#C00",
-    "success-light": "#3291FF",
-    "success": "#0070F3",
-    "success-dark": "#0366D6",
-    "warning-light": "#F7B955",
-    "warning": "#F5A623",
-    "warning-dark": "#F49B0B",
-    "alert": "#FF0080",
-    "purple": "#F81CE5",
-    "violet": "#7928CA",
-    "cyan": "#79FFE1",
-  }
-
-  let utilities = {}
-
-  for (let [key, value] of Object.entries(colors)) {
-    utilities[`.bg-${key}`] = {
-      backgroundColor: value
+module.exports = {
+  config: {
+    theme: {
+      extend: {
+        colors: {
+          "accent-1": "#FAFAFA",
+          "accent-2": "#EAEAEA",
+          "accent-3": "#999",
+          "accent-4": "#888",
+          "accent-5": "#666",
+          "accent-6": "#444",
+          "accent-7": "#333",
+          "accent-8": "#111",
+          "error-light": "#FF1A1A",
+          "error": "#E00",
+          "error-dark": "#C00",
+          "success-light": "#3291FF",
+          "success": "#0070F3",
+          "success-dark": "#0366D6",
+          "warning-light": "#F7B955",
+          "warning": "#F5A623",
+          "warning-dark": "#F49B0B",
+          "alert": "#FF0080",
+          "purple": "#F81CE5",
+          "violet": "#7928CA",
+          "cyan": "#79FFE1"
+        }
+      }
     }
   }
-
-  addUtilities(utilities, ['responsive', 'hover'])
 }
